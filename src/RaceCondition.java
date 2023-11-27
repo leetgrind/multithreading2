22a71743073a0cbc5eb71357d6332922d59efdbc
leetgrind <leetgrind.org@gmail.com>
2023-11-27 18:37:58 -0500

    added synchronized

---
+++ src/RaceCondition.java
@@ -0,0 +1,34 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class RaceCondition {
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+
+        Counter counter = new Counter();
+
+        int numberOfTasks = 100;
+
+        for(int i=0; i<numberOfTasks; i++) {
+            executorService.submit(counter::increase);
+        }
+
+        executorService.shutdown();
+
+        System.out.println("Counter: " + counter.getCount());
+
+    }
+}
+
+class Counter {
+
+    private int count = 0;
+
+    public synchronized void increase() {
+        count++;
+    }
+    public int getCount() {
+        return count;
+    }
+}
